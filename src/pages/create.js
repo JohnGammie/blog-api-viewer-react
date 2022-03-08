64f169cba8426d3683018c307ebed8c56ea5e834
JohnGammie <gammie@y7mail.com>
2022-03-09 02:09:09 +1100

    Add Create new Post form

--- src/pages/create.js
+++ src/pages/create.js
@@ -1,7 +1,54 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./style.css";
 
 const Create = () => {
-  return <div>Create</div>;
+  const navigate = useNavigate();
+  const createPost = async (e) => {
+    // Disabling the "Submit" behaviour will also stop simple HTML required validation.
+    // Force check the validity here, as it is a simple validation suitable for this program
+    const form = document.querySelector(".createPostForm");
+    if (form.checkValidity()) {
+      e.preventDefault();
+
+      const authorInput = document.getElementById("authorInput");
+      const titleInput = document.getElementById("titleInput");
+      const contentInput = document.getElementById("contentInput");
+
+      const port = 3000;
+      const serverUrl = "http://localhost:" + port;
+      await fetch(serverUrl + "/post/create", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          author: authorInput.value,
+          title: titleInput.value,
+          content: contentInput.value,
+        }),
+      }).then((data) => {
+        if (data.status === 200) {
+          navigate("/posts");
+        }
+      });
+    }
+  };
+
+  return (
+    <form action="" className="createPostForm">
+      <label htmlFor="author">Author</label>
+      <input type="text" name="author" id="authorInput" required />
+      <label htmlFor="title">Title</label>
+      <input type="text" name="title" id="titleInput" required />
+      <label htmlFor="content">Content</label>
+      <input type="text" name="content" id="contentInput" required />
+      <button type="submit" className="createPostButton" onClick={createPost}>
+        Create Post
+      </button>
+    </form>
+  );
 };
 
 export default Create;
